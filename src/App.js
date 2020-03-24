b99e3fc9945b3545abf4b65b1a7f1d0012e08433
_krit <kritmadaan@gmail.com>
2020-03-24 03:04:40 -0700

    my

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React from 'react';
-
-import Content from './components/MyContent';
 import MyGallery from './components/MyGalley';
 import Footer from './components/Footer';
 import Myheader from './components/Myheader';
 import Testinomial from './components/Testinomial';
 import Featured from './components/Featured';
-import Form from './components/Form';
 import MyContent from "./components/MyContent";
 import Slider from './components/Myheader/Slider';
 
 function App() {
   return (
     <div className="App">
-    <Myheader/>
-  
-    <Featured/>
-    <Slider/>
-    <MyGallery/>
-    <MyContent/>
-  
-   
-    {/* <Myheader/>
-     <Featured/>
-      
-     <MyContent/>*/}
-      
-      <Testinomial/>
-    
-      <Footer/>
-  
+      <Myheader />
+      <Featured />
+      <Slider />
+      <MyGallery />
+      <MyContent />
+      <Testinomial />
+      <Footer />
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMuiTheme,} from '@material-ui/core/styles';
+
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
  import'bootstrap-css-only/css/bootstrap.min.css'; 
  import 'mdbreact/dist/css/mdb.css';
 
-const theme =createMuiTheme()
 ReactDOM.render(
-  <createMuiTheme>
+
     <App />
-    </createMuiTheme>,
+    ,
   document.getElementById('root')
 );
 
